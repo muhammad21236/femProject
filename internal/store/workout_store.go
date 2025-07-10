3cedd166901b8abbc949f23f95d563d631c95362
Muhammad <155086759+muhammad21236@users.noreply.github.com>
2025-07-10 20:14:17 +0500

    Defining Data Types in Store

--- internal/store/workout_store.go
+++ internal/store/workout_store.go
@@ -1,1 +1,36 @@
 package store
+
+import "database/sql"
+
+type Workout struct {
+	ID             int            `json:"id"`
+	Title          string         `json:"title"`
+	Description    string         `json:"description"`
+	Duration       int            `json:"duration"`        // Duration in minutes
+	CaloriesBurned int            `json:"calories_burned"` // Calories burned
+	Entries        []WorkoutEntry `json:"entries"`         // Date of the workout in YYYY-MM-DD format
+}
+
+type WorkoutEntry struct {
+	ID              int      `json:"id"`
+	ExerciseName    string   `json:"exercise_name"`    // Name of the exercise
+	Sets            int      `json:"sets"`             // Number of sets
+	Reps            *int     `json:"reps"`             // Number of repetitions
+	DurationSeconds *int     `json:"duration_seconds"` // Duration of the exercise in seconds
+	Weight          *float64 `json:"weight"`           // Weight used in kg
+	Notes           string   `json:"notes"`            // Additional notes for the entry
+	OrderIndex      int      `json:"order_index"`      // Order index for sorting entries
+}
+
+type PostgresWorkoutStore struct {
+	db *sql.DB
+}
+
+func NewPostgresWorkoutStore(db *sql.DB) *PostgresWorkoutStore {
+	return &PostgresWorkoutStore{db: db}
+}
+
+type WorkoutStore interface {
+	CreteWorkout(*Workout) (*Workout, error)
+	GetWorkoutByID(id int64) (*Workout, error)
+}
