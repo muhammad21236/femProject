efcfc1eea6c8a9c73096fc615f1729106b21faf6
Muhammad <155086759+muhammad21236@users.noreply.github.com>
2025-07-16 21:44:23 +0500

    User SQL Queries

--- internal/store/user_store.go
+++ internal/store/user_store.go
@@ -4,7 +4,7 @@ import (
 	"database/sql"
 	"time"
 
-	_"golang.org/x/crypto/bcrypt"
+	_ "golang.org/x/crypto/bcrypt"
 )
 
 type password struct {
@@ -34,4 +34,49 @@ type UserStore interface {
 	CreateUser(*User) error
 	GetUserByUsername(username string) (*User, error)
 	UpdateUser(*User) error
+}
+
+func (s *PostgresUserStore) CreateUser(user *User) error {
+	query := `INSERT INTO users (username, email, password_hash, bio, created_at, updated_at)
+			  VALUES ($1, $2, $3, $4, $5, $6)
+			  RETURNING id, created_at, updated_at`
+	err := s.db.QueryRow(query, user.Username, user.Email, user.PasswordHash.hash, user.Bio, time.Now(), time.Now()).Scan(&user.ID, &user.CreatedAt, &user.UpdatedAt)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *PostgresUserStore) GetUserByUsername(username string) (*User, error) {
+	user := &User{
+		PasswordHash: password{},
+	}
+	query := `SELECT id, username, email, password_hash, bio, created_at, updated_at
+			  FROM users WHERE username = $1`
+	err := s.db.QueryRow(query, username).Scan(&user.ID, &user.Username, &user.Email, &user.PasswordHash.hash, &user.Bio, &user.CreatedAt, &user.UpdatedAt)
+	if err == sql.ErrNoRows {
+		return nil, nil // User not found
+	}
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (s *PostgresUserStore) UpdateUser(user *User) error {
+	query := `UPDATE users SET username = $1, email = $2, bio = $3, updated_at = CURRENT_TIMESTAMP
+			  WHERE id = $4
+			  RETURNING updated_at`
+	result, err := s.db.Exec(query, user.Username, user.Email, user.Bio, time.Now(), user.ID)
+	if err != nil {
+		return err
+	}
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected == 0 {
+		return sql.ErrNoRows // No user found with the given ID
+	}
+	return nil
 }
