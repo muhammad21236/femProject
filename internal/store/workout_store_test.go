6683824202f32928a13db7f85a2210a72a3e7d5c
Muhammad <155086759+muhammad21236@users.noreply.github.com>
2025-07-16 17:43:26 +0500

    Testing Create Workout Success

--- internal/store/workout_store_test.go
+++ internal/store/workout_store_test.go
@@ -2,7 +2,7 @@ package store
 
 import (
 	"database/sql"
-	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 	"testing"
 )
 
@@ -21,4 +21,54 @@ func setupTestDB(t *testing.T) *sql.DB {
 		t.Fatalf("truncating test db: %v", err)
 	}
 	return db
+}
+
+func TestCreateWorkout(t *testing.T) {
+	db := setupTestDB(t)
+	defer db.Close()
+
+	store := NewPostgresWorkoutStore(db)
+	tests := []struct {
+		name     string
+		workout  *Workout
+		wantErr  bool
+	}{
+		{
+			name: "Create valid workout",
+			workout: &Workout{
+				Title:              "Test Workout",
+				Description: "This is a test workout",
+				DurationMinutes:          60,
+				CaloriesBurned:    200,
+				Entries: []WorkoutEntry{
+					{
+						ExerciseName: "Push-ups",
+						Reps:         IntPtr(10),
+						Sets:         3,
+						Weight:       FloatPtr(135.5),
+						Notes:        "Good form",
+						OrderIndex:   1,
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := store.CreteWorkout(tt.workout)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CreateWorkout() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func IntPtr(i int) *int {
+	return &i
+}
+
+func FloatPtr(i float64) *float64 {
+	return &i
 }
