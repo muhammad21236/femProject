bb7107cfab0f32de861bb61931b93cede402ca34
Muhammad <155086759+muhammad21236@users.noreply.github.com>
2025-07-20 04:36:37 +0500

    Authentication Middleware

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -3,8 +3,11 @@ package middleware
 import (
 	"context"
 	"net/http"
+	"strings"
 
 	"github.com/muhammad21236/femProject/internal/store"
+	"github.com/muhammad21236/femProject/internal/tokens"
+	"github.com/muhammad21236/femProject/internal/utils"
 )
 
 type UserMiddleware struct {
@@ -26,4 +29,36 @@ func GetUser(r *http.Request) *store.User {
 		panic("Missing User In Request")
 	}
 	return user
+}
+
+func (um *UserMiddleware) Authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Authorization")
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			r = SetUser(r, store.AnonymousUser)
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		headerParts := strings.Split(authHeader, " ")
+		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+			utils.WriteJSON(w, http.StatusUnauthorized, utils.Envelope{"error": "Invalid Authorization Header"})
+			return
+		}
+
+		token := headerParts[1]
+		user, err := um.UserStore.GetUserToken(tokens.ScopeAuth, token)
+		if err != nil {
+			utils.WriteJSON(w, http.StatusUnauthorized, utils.Envelope{"error":"Invalid Token"})
+			return
+		}
+		if user == nil {
+			utils.WriteJSON(w, http.StatusUnauthorized, utils.Envelope{"error":"Token Expired or Invalid"})
+			return
+		}
+
+		r = SetUser(r, user)
+		next.ServeHTTP(w, r)
+	})
 }
